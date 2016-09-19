53f0a52d4b31ab5335cb196d1c9f06634a5770ed
Fabian Fritz <fabianfuture@web.de>
2016-09-19 12:04:49 +0200

    Add option to plot curvature gradient

--- plot.py
+++ plot.py
@@ -15,12 +15,20 @@ if __name__ == '__main__':
 		print 'Please provide as an argument the file you want to plot'
 		sys.exit(1)
 	shape = Sphere(sys.argv[1],150)
-	if len(sys.argv) > 3 and sys.argv[2] == 'curvature':
+	if len(sys.argv) == 2:
+		sys.argv.append('error')
+
+	if sys.argv[2] == 'curvature':
 		curvature = shape.getCurvature()
 		plt.hist(curvature,bins=50,normed=1)
 		plt.title("Curvature")
 		plt.show()
 		shape.render(curvature)
+	elif sys.argv[2] == 'gradient':
+		curvature = shape.getCurvature()
+		grad = np.gradient(curvature)
+		shape.render(curvature)
+		shape.render(grad)
 	else:
 		error = np.abs(shape.fittedRadius - distance(shape.vertices,shape.centerPoint))
 		plt.hist(error,bins=50,normed=1)
