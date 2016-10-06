2e2dd2b8db24a7875ab9fa30c97c75349b27d8ac
Fabian Fritz <fabianfuture@web.de>
2016-10-06 14:41:45 +0200

    Start using sparse matrices

--- spherical_harmonics.py
+++ spherical_harmonics.py
@@ -1,12 +1,15 @@
 #! /usr/bin/python
 
 
-
+import os
 import sys
 import timeit
 import numpy as np
+from cycler import cycler
 from math import isnan
 from scipy.special import sph_harm
+from scipy.sparse import lil_matrix, hstack
+from scipy.sparse.linalg import lsqr
 from src.OBJIO import loadOBJ
 from src.fitting import fitSphere
 from src.mesh import Mesh
@@ -36,7 +39,8 @@ def residuals(aa, Yval, rs):
 
 def getAMatrix(theta, phi, l):
 	n = len(phi)
-	A = np.zeros((n,2*l+1))
+	#A = np.zeros((n,2*l+1))
+	A = lil_matrix((n, 2*l+1))
 	for i in range(n):
 		for j in range(0,2*l+1):
 			s = sph_harm(l, j-l, theta[i], phi[i]).real
@@ -50,35 +54,31 @@ def sirf(sphericalCoordinates, LMax):
 	s = 5
 	A = getAMatrix(theta, phi, 0)
 	for i in range(1,s):
-		A = np.concatenate((A,getAMatrix(theta, phi, i)), axis=1)
+		A = hstack([A,getAMatrix(theta, phi, i)])
 	
 	b0 = np.zeros(A.shape[1])
-	b, flag = leastsq(residuals, b0, args=(A,r))
-
+	#b, flag = leastsq(residuals, b0, args=(A,r))
+	b = lsqr(A,r)[0]
 
 	res = r - A.dot(b)
+	b.resize((Lmax+1)**2)
 	for l in range(s, Lmax+1):
 		Al = getAMatrix(theta, phi, l)
-		bl0 = np.zeros(Al.shape[1])
-
-		bl, flag = leastsq(residuals, bl0, args=(Al,res))
-		res = res - Al.dot(bl)
-
 		lowerBound = l**2
 		upperBound = (l+1)**2
-		print lowerBound
-		print upperBound
-		print len(bl)
-		b = np.concatenate((b, bl))
-	print len(b)
 
-	LMatrix = np.zeros((Lmax+1, Lmax*2+1))
+		#b[lowerBound:upperBound], flag = leastsq(residuals, b[lowerBound:upperBound], args=(Al,res))
+		b[lowerBound:upperBound] = lsqr(Al,res)[0]
+		res = res - Al.dot(b[lowerBound:upperBound])
+
+
+	#LMatrix = np.zeros((Lmax+1, Lmax*2+1))
 	ys = np.zeros(Lmax+1)
 	for i in range(Lmax+1):
 		lowerBound = i**2
 		upperBound = (i+1)**2
-		LMatrix[i, 0:(upperBound-lowerBound)] = b[lowerBound:upperBound]
-		ys[i] = np.linalg.norm(LMatrix[i, :], ord=2)
+		#LMatrix[i, 0:(upperBound-lowerBound)] = b[lowerBound:upperBound]
+		ys[i] = np.linalg.norm(b[lowerBound:upperBound], ord=2)
 		#print 'from ' + str((i)**2)
 		#print 'to ' + str((i+1)**2)
 	
@@ -117,74 +117,47 @@ def nlsf(sphericalCoordinates, LMax):
 
 	return a1, ys, r_approx
 
+def loadOBJwithSphericalCoordinates(fileName):
+	vertices, faces, normals = loadOBJ(fileName)
+	bounds = Mesh(vertices.T, faces, normals).getBounds()
+	p0 = [bounds[0][0],bounds[1][0],bounds[2][0],150]
+	centerPoint, radius = fitSphere(vertices, p0, 150, bounds)
+
+	sphericalCoordinates = getSphericalCoordinates(vertices, centerPoint)
+
+	return sphericalCoordinates
+
 if __name__ == '__main__':
 	if len(sys.argv) < 3:
 		print "Please specify OBJPATH NOFREQUENCIES"
 		sys.exit(0)
-	vertices, faces, normals = loadOBJ(sys.argv[1])
 
-	bounds = Mesh(vertices.T, faces, normals).getBounds()
-	p0 = [bounds[0][0],bounds[1][0],bounds[2][0],150]
-
-	centerPoint, radius = fitSphere(vertices, p0, 150, bounds)
-	
-	#fig = plt.figure()
-	#ax = fig.add_subplot(111, projection='3d')
-	#ax.scatter(vertices.T[0], vertices.T[1], vertices.T[2])
-	#ax.scatter(centerPoint[0], centerPoint[1], centerPoint[2], color='r')
-	#plt.show()
-
-	# Create a sphere
-	pi = np.pi
-	cos = np.cos
-	sin = np.sin
-	#phi = np.linspace(0,pi,101)
-	#theta = np.linspace(-pi,pi,101)
-	sphericalCoordinates = getSphericalCoordinates(vertices, centerPoint)
 	Lmax = int(sys.argv[2])
-	phi, theta, r = sphericalCoordinates
 
-	#a1, ys, r_approx = nlsf(sphericalCoordinates, Lmax)
-	
-	#a2, ys2 = sirf(sphericalCoordinates, Lmax)
-	start_time = timeit.default_timer()
-	nlsf(sphericalCoordinates, Lmax)
-	print(timeit.default_timer() - start_time)
+	folderPath = sys.argv[1]
+	files = os.listdir(folderPath)
+	numColors = len(files)
 
-	start_time = timeit.default_timer()
-	sirf(sphericalCoordinates, Lmax)
-	print(timeit.default_timer() - start_time)
+	cm = plt.get_cmap('gist_rainbow')
 
-	sys.exit(0)
-
-	#vertices2 = getCartesianCoordinates(theta, phi, r_approx, centerPoint)
-	#rmse = np.linalg.norm(r - r_approx) / np.sqrt(len(a1))
-
-	xplot = np.arange(0, len(a2), 1)
 	fig = plt.figure()
-	#plt.plot(xplot, a1)
-	#a1 = np.fabs(a1)
-	#a1 = np.sort(a1)[::-1]
-	#a1 = a1[a1 > 0]
-	#print a1.max()
+	ax = fig.add_subplot(111)
+	ax.set_prop_cycle(cycler('color',[cm(1.*(i*10)/numColors) for i in range(numColors)]))
+	for fileName in files:
+		if fileName.endswith('.obj'):
+			sphericalCoordinates = loadOBJwithSphericalCoordinates(folderPath + fileName)
 
-	xa = np.arange(0, len(ys2))
-	#plt.plot(xa, ys)
-	plt.plot(xa, ys2, color='r')
+			start_time = timeit.default_timer()
+			a, ys = sirf(sphericalCoordinates, Lmax)
+			print(timeit.default_timer() - start_time)
+
+			xa = np.arange(0, len(ys))
+			plt.plot(xa, ys, label=fileName[0:-11])
+			break
+
+			#rmse = np.linalg.norm(r - r_approx) / np.sqrt(len(a1))
+
+	plt.legend()
 	plt.grid(True)
-	plt.title(sys.argv[1])
 	plt.gca().set_yscale('log')
 	plt.show()
-
-	#bins = np.arange(a1.min(),a1.max(),len(a1))
-	#bins = 10**np.linspace(np.log10(0.00001), np.log10(600),50)
-	#bins = np.logspace(0.00000001, 0.000001, 50)
-	#plt.xticks(bins, ["2^%s" % i for i in bins])
-	#plt.hist(a1, bins=bins)
-	#plt.hist(a1, bins=np.arange(a1.min(), a1.max()-1))
-	
-	#plt.gca().set_xscale('log')
-
-
-	#x2plot = np.arange(0, len(rmses), 1)
-	#plt.plot(x2plot, rmses)
